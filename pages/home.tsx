1573e34b1e1b054a30e2b9773e2ac49c9583a74e
Gabriel Nascimento <gsnaxsi@gmail.com>
2021-12-03 12:47:00 -0300

    :page_facing_up: App dedicated page

---
+++ pages/home.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+import App from '../components/app/App';
+
+const Home: NextPage = () => {
+    return (
+        <section>
+            <title>yNotes :: Home Page</title>
+            <App />
+        </section>
+    )
+};
+
+export default Home
