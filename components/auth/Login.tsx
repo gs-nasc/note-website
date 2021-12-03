e34023c264bed65feb19a0817d87a5f60730f50e
Gabriel Nascimento <gsnaxsi@gmail.com>
2021-12-03 10:26:47 -0300

    :lock: Login Screen

--- components/auth/Login.tsx
+++ components/auth/Login.tsx
@@ -1,51 +1,62 @@
 import { NextPage } from "next";
-import styles from './style.module.css'
+import { useState } from "react";
+import Illustration from './Illustration';
+import { MailIcon, LockClosedIcon, CodeIcon } from '@heroicons/react/outline';
 
 const Login: NextPage = () => {
+    const [color, setColor] = useState('#34d399');
+
     return (
-        <section className="absolute w-full top-0">
-            <div className="container mx-auto px-4 h-full">
-                <div className="flex content-center items-center justify-center h-full">
-                    <div className="w-full lg:w-4/12 px-4 pt-32">
-                        <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300 border-0">
-                            <div className="rounded-t mb-0 px-6 py-6">
-                                <div className="text-center mb-3">
-                                    <h6 className="text-gray-600 text-sm font-bold">Sign in with</h6></div>
-                                <div className="btn-wrapper text-center">
-                                    {/* <button className="bg-white active:bg-gray-100 text-gray-800 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs" type="button"><img alt="..." className="w-5 mr-1" src="https://demos.creative-tim.com/tailwindcss-starter-project/static/media/github.4ffd4fe7.svg">Github</button>
-                                    <button className="bg-white active:bg-gray-100 text-gray-800 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs" type="button" style="transition: all 0.15s ease 0s;"><img alt="..." className="w-5 mr-1" src="https://demos.creative-tim.com/tailwindcss-starter-project/static/media/google.87be59a1.svg">Google</button> */}
-                                </div>
-                                <hr className="mt-6 border-b-1 border-gray-400" />
-                            </div>
-                            <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
-                                <div className="text-gray-500 text-center mb-3 font-bold"><small>Or sign in with credentials</small></div>
-                                <form>
-                                    <div className="relative w-full mb-3">
-                                        <label className="block uppercase text-gray-700 text-xs font-bold mb-2" htmlFor="grid-password">Email</label>
-                                        <input type="email" className={`px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ${styles.transition}`} placeholder="Email" />
-                                    </div>
-                                    <div className="relative w-full mb-3">
-                                        <label className="block uppercase text-gray-700 text-xs font-bold mb-2" htmlFor="grid-password">Password</label>
-                                        <input type="password" className={`px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ${styles.transition}`} placeholder="Password" />
-                                    </div>
-                                    <div>
-                                        <label className="inline-flex items-center cursor-pointer">
-                                            <input id="customCheckLogin" type="checkbox" className={`htmlForm-checkbox text-gray-800 ml-1 w-5 h-5 ${styles.transition}`} />
-                                            <span className="ml-2 text-sm font-semibold text-gray-700">Remember me</span></label>
-                                    </div>
-                                    <div className="text-center mt-6">
-                                        <button className={`bg-gray-900 text-white active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ${styles.transition}`} type="button">Sign In</button>
-                                    </div>
-                                </form>
-                            </div>
+        <section className="w-full min-h-screen top-0 bg-gray-900 flex flex-col justify-center items-center">
+            <div className="w-3/4 flex flex-row mt-auto">
+                <div className="w-1/2 rounded-l-3xl flex flex-row justify-center items-center px-16 py-40" style={{ background: color, transition: 'all', transitionDuration: '0.1s' }}>
+                    <Illustration color={color} height="auto" width="60%" />
+                </div>
+                <div className="w-1/2 bg-gray-700 rounded-r-3xl px-16 flex flex-row items-center justify-center">
+                    <div className="w-3/4">
+                        <div className="text-white text-3xl font-bold">
+                            Login
                         </div>
-                        <div className="flex flex-wrap mt-6">
-                            <div className="w-1/2"><a href="#pablo" className="text-gray-300"><small>htmlForgot password?</small></a></div>
-                            <div className="w-1/2 text-right"><a href="#pablo" className="text-gray-300"><small>Create new account</small></a></div>
+                        <section>
+                            <div className="mt-5 flex flex-row items-center bg-gray-200 rounded-lg">
+                                <input type="text"
+                                    className="w-full pl-4 pr-10 py-3 leading-none shadow-sm focus:outline-none focus:shadow-outline text-gray-700 bg-transparent"
+                                    placeholder="Email" id="email" />
+                                <label htmlFor="email">
+                                    <MailIcon className="h-5 w-5 mr-3 text-gray-700 font-bold" />
+                                </label>
+                            </div>
+                            <div className="mt-5 flex flex-row items-center bg-gray-200 rounded-lg">
+                                <input type="text"
+                                    className="w-full pl-4 pr-10 py-3 leading-none rounded-lg shadow-sm focus:outline-none focus:shadow-outline text-gray-700 bg-transparent"
+                                    placeholder="Password" id="password" />
+                                <label htmlFor="password">
+                                    <LockClosedIcon className="h-5 w-5 mr-3 text-gray-700 font-bold" />
+                                </label>
+                            </div>
+                        </section>
+                        <div className="flex flex-row justify-between items-center">
+                            <button className="mt-5 px-4 py-2 rounded-md text-sm font-medium border-b-2 focus:outline-none focus:ring transition text-white bg-green-500 border-green-800 hover:bg-green-600 active:bg-green-700 focus:ring-green-300" type="submit">Login</button>
+                            <button className="mt-5 px-4 py-2 rounded-md text-sm font-medium border-b-2 focus:outline-none focus:ring transition text-white bg-green-500 border-green-800 hover:bg-green-600 active:bg-green-700 focus:ring-green-300" type="submit">Register</button>
                         </div>
                     </div>
                 </div>
             </div>
+            <footer className="mt-auto">
+                <div className="w-full flex flex-row justify-center items-center py-3">
+                    <div className="text-center">
+                        <p className="text-gray-500 hover:text-gray-400 cursor-pointer transition-all text-opacity-50">{new Date().getFullYear()} © {process.env.NEXT_PUBLIC_APP_NAME} yNotes - All directs reserved</p>
+                        <p className="text-center  flex items-center justify-center">
+                            <a href="https://github.com/gs-nasc" target="_blank">
+                                <svg width="24" height="24" fill="currentColor" className="text-gray-500 hover:text-gray-400 text-opacity-50 mx-1"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 2C6.477 2 2 6.463 2 11.97c0 4.404 2.865 8.14 6.839 9.458.5.092.682-.216.682-.48 0-.236-.008-.864-.013-1.695-2.782.602-3.369-1.337-3.369-1.337-.454-1.151-1.11-1.458-1.11-1.458-.908-.618.069-.606.069-.606 1.003.07 1.531 1.027 1.531 1.027.892 1.524 2.341 1.084 2.91.828.092-.643.35-1.083.636-1.332-2.22-.251-4.555-1.107-4.555-4.927 0-1.088.39-1.979 1.029-2.675-.103-.252-.446-1.266.098-2.638 0 0 .84-.268 2.75 1.022A9.606 9.606 0 0112 6.82c.85.004 1.705.114 2.504.336 1.909-1.29 2.747-1.022 2.747-1.022.546 1.372.202 2.386.1 2.638.64.696 1.028 1.587 1.028 2.675 0 3.83-2.339 4.673-4.566 4.92.359.307.678.915.678 1.846 0 1.332-.012 2.407-.012 2.734 0 .267.18.577.688.48C19.137 20.107 22 16.373 22 11.969 22 6.463 17.522 2 12 2z"></path></svg>
+                            </a>
+                            <a href="https://github.com/gs-nasc/note-website" target="_blank">
+                                <CodeIcon className="h-5 w-5 text-gray-500 text-opacity-50 mx-1 hover:text-gray-400" />
+                            </a>
+                        </p>
+                    </div>
+                </div>
+            </footer>
         </section>
     )
 }
